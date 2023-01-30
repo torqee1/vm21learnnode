9b6c82ce395535d896b7a23022680347e777712c
OLIVER KAUR <u-49@tptlive.ee>
2023-01-30 15:07:57 +0200

    add templating

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('index.njk');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about.njk');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact.njk');
+});
+
+app.get('/gallery', (req, res) => {
+  res.render('gallery.njk');
+});
+
+app.get('/values', (req, res) => {
+  res.render('values.njk');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
